28c275664442e88a37d62839f0cb32c9944ec5b2
jmeldrum <jamiewmeldrum@gmail.com>
2025-08-11 12:48:00 +0100

    Added function in stats to build character count dict. Used in main to print dict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,9 @@ def get_book_text(filepath):
 def main():
    words = get_book_text("books/frankenstein.txt")
    word_count = get_word_count(words)
-   print(f"{word_count} words found in the document")
+   character_count_dict = get_character_count_dict(words)
 
+   print(f"{word_count} words found in the document")
+   print(f"{character_count_dict} characters found")
+   
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def get_word_count(text):
     words = str.split(text)
     return len(words)
+
+def get_character_count_dict(text):
+
+    character_dict = {}
+
+    characters = list(text)
+    for character in characters:
+        character = str.lower(character)
+
+        count = character_dict.get(character)
+        if count is None:
+            count = 0
+        character_dict.update({character: count + 1})
+
+    return character_dict
