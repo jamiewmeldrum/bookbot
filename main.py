72c0838d5b6d468d507d59e38ecd2bb65e27c943
jmeldrum <jamiewmeldrum@gmail.com>
2025-08-11 13:25:07 +0100

    Reworked to return list of characters, where character is dict with char and num keys. Added functions to sort this list on num count and filter non-alphabet. Applied in main to produce report

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from stats import get_word_count, get_character_count_dict
+from stats import get_word_count, get_characters, sort_characters, filter_non_alphabet
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     return None
 
-def main():
-   words = get_book_text("books/frankenstein.txt")
-   word_count = get_word_count(words)
-   character_count_dict = get_character_count_dict(words)
+def print_characters(characters):
+    print("--------- Character Count -------")
 
-   print(f"{word_count} words found in the document")
-   print(f"{character_count_dict} characters found")
+    for character in characters:
+        char = character["char"]
+        num = character["num"]
+        print(f"{char}: {num}")
+
+def print_report(book, word_count, characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print_characters(characters)
+    print("============= END ===============")
+
+def main():
+   book = "books/frankenstein.txt"
+   words = get_book_text(book)
+   word_count = get_word_count(words)
+   characters = get_characters(words)
+   characters = filter_non_alphabet(characters)
+   sort_characters(characters)
+   print_report(book, word_count, characters)
    
 main()
--- stats.py
+++ stats.py
@@ -2,16 +2,42 @@ def get_word_count(text):
     words = str.split(text)
     return len(words)
 
-def get_character_count_dict(text):
+def get_characters(text):
 
-    character_dict = {}
+    character_count_dict = {}
 
     for character in text:
         character = character.lower()
 
-        if character in character_dict:
-            character_dict[character] += 1
+        if character in character_count_dict:
+            character_count_dict[character] += 1
         else:
-            character_dict[character] = 1
+            character_count_dict[character] = 1
 
-    return character_dict
+    characters = []
+    for character in character_count_dict:
+        characters.append(
+            {
+                "char": character,
+                "num": character_count_dict[character]
+            }
+        )
+
+    return characters
+
+
+def characters_sort_on(items):
+    return items["num"]
+
+
+def sort_characters(characters):
+    characters.sort(reverse=True, key=characters_sort_on)
+
+
+def filter_non_alphabet(characters):
+    filtered_characters = []
+    for character in characters:
+        if character["char"].isalpha():
+            filtered_characters.append(character)
+
+    return filtered_characters
