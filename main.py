0468008469b5ded49577a849873661a256b19ecf
jmeldrum <jamiewmeldrum@gmail.com>
2025-08-11 12:39:35 +0100

    Updated gitignore to ignore pycache. Refactored main.py to extract word count function into stats.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     return None
-
-def get_word_count(text):
-    words = str.split(text)
-    return len(words)
 
 def main():
    words = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(text):
+    words = str.split(text)
+    return len(words)
